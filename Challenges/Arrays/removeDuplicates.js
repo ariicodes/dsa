c10e797c0056f95cf83ebc1a27b2ac6c61d69e92
Ariela Israel <arielaisrael12@gmail.com>
2024-01-16 22:29:59 -0500

    complete

--- Challenges/Arrays/removeDuplicates.js
+++ Challenges/Arrays/removeDuplicates.js
@@ -1,1 +1,23 @@
 // Write a function to remove duplicates from an array and return the unique elements.
+
+const removeDuplicates = arr => {
+	const uniqueElements = [];
+
+	arr.forEach(e => {
+		if (!uniqueElements.includes(e)) {
+			uniqueElements.push(e);
+		}
+	});
+
+	return uniqueElements;
+};
+
+const inputArray = [1, 2, 2, 3, 4, 4, 5, 12, 12, 15, 16, 12, 18, 19];
+const resultArray = removeDuplicates(inputArray);
+
+console.log(resultArray);
+
+/**
+ * TIME COMPLEXITY: O(n^2)
+ * SPACE COMPLEXITY: O(n)
+ */
--- Challenges/Arrays/rotateArray.js
+++ Challenges/Arrays/rotateArray.js
@@ -1,1 +1,21 @@
 // Implement a function to rotate the elements of an array to the right by a given number of steps.
+
+const rotateArray = (steps, arr) => {
+  const leftArr = arr.slice(0, steps)
+  const results = arr.slice(steps)
+
+  while (leftArr.length > 0) {
+    results.push(leftArr.pop())
+  }
+
+  return results
+}
+
+const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const results = rotateArray(8, inputArray)
+
+console.log(results)
+
+/**
+ * TIME & SPACE COMPLEXITY: O(n)
+ */
